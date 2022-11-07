7d16f20c96795709af780d012558862e59586290
ionically <chandnichaudhari.66@gmail.com>
2022-11-07 16:08:11 +0530

    Initial Commit

---
+++ FruitQuizUIStoryBoard/FruitQuizUIStoryBoard/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FruitQuizUIStoryBoard
+//
+//  Created by chandni chaudhari on 07/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func actionplay(_ sender: Any) {
+    }
+}
+
