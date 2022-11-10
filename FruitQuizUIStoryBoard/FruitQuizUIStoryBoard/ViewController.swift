42384dde6a0eeeeb3c1d69db3a9a4c85ceb154ba
ionically <chandnichaudhari.66@gmail.com>
2022-11-10 17:54:39 +0530

    Added Logic for next and previous Button

--- FruitQuizUIStoryBoard/FruitQuizUIStoryBoard/ViewController.swift
+++ FruitQuizUIStoryBoard/FruitQuizUIStoryBoard/ViewController.swift
@@ -48,13 +48,16 @@ class ViewController: UIViewController {
         showCross()
     }
     
-    // MARK: Add Code here
+    // MARK: Add Code here : done
     // make @IBAction for previous button
     // make @IBAction for next button
     
     @IBAction func previousButton(_ sender: UIButton) {
+        previousLogic()
     }
     @IBAction func nextButton(_ sender: Any) {
+        nextLogic()
+        
     }
     
     func showTick() {
@@ -77,42 +80,57 @@ class ViewController: UIViewController {
         let quiz1 = SmartQuiz(fruit: "Apple", buttonALabel: "GrapeFruit", buttonMLabel: "Apple", buttonOLabel: "Orange")
         let quiz2 = SmartQuiz(fruit: "Mango", buttonALabel: "Grapes", buttonMLabel: "Mango", buttonOLabel: "Orange")
         let quiz3 = SmartQuiz(fruit: "Orange", buttonALabel: "Apple", buttonMLabel: "Orange", buttonOLabel: "Grapes")
-        arrayQuiz.append(quiz1)
+        arrayQuiz.append(quiz1)//insert
         arrayQuiz.append(quiz2)
         arrayQuiz.append(quiz3)
     }
     
     func loadFirstQuiz() {
         let quiz1 = arrayQuiz[0]
+        FruitImageView.image = UIImage(named: quiz1.fruit)
         optionOButton.setTitle(quiz1.buttonOLabel, for: .normal)
         optionAButton.setTitle(quiz1.buttonALabel, for: .normal)
         optionMButton.setTitle(quiz1.buttonMLabel, for: .normal)
     }
     
+    func loadNextPreviousQuiz(index: Int) {
+        let change = arrayQuiz[index]
+        FruitImageView.image = UIImage(named: change.fruit)
+        optionOButton.setTitle(change.buttonOLabel, for: .normal)
+        optionAButton.setTitle(change.buttonALabel, for: .normal)
+        optionMButton.setTitle(change.buttonMLabel, for: .normal)
+    }
+    
+    
     func handleLogicNextPreviousButtons() {
         if currentQuizIndex == firstIndex {
-            // MARK:
-            hidepreviousButton()
-            shownextButton()
+            // MARK: done
+            previousButton.isHidden = true
+           
         }  else if currentQuizIndex == lastIndex {
-            // TODO:
-            showpreviousButton()
-            hidenextButton()
+            // MARK: done
+            nextButton.isHidden = true
+            
         } else {
-            // MARK:
-            showpreviousButton()
-            shownextButton()
+            // MARK: done
+            nextButton.isHidden = false
+            previousButton.isHidden = false
+  
         }
     }
     
     // TODO: call this function inside @IBAction of next button
     func nextLogic() {
-        currentQuizIndex > 1
+    currentQuizIndex += 1
+        loadNextPreviousQuiz(index: currentQuizIndex)
+        handleLogicNextPreviousButtons()
     }
     
     // TODO: call this function inside @IBAction of previous button
     func previousLogic() {
-        currentQuizIndex < 1
+        currentQuizIndex -= 1
+            loadNextPreviousQuiz(index: currentQuizIndex)
+        handleLogicNextPreviousButtons()
     }
     
 }
@@ -123,3 +141,4 @@ struct SmartQuiz {
     var buttonMLabel: String
     var buttonOLabel: String
 }
+
